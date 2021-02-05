cb89de08bd01e4cdf65a60e36a911850260d272a
Kenneth Smith <ksmith@c2b2.columbia.edu>
2021-02-05 15:59:40 -0500

    add detection of embedded line feeds in strings
    add detection of embedded line feeds in strings and fix source data

--- src/generate_HIPC_submissions.R
+++ src/generate_HIPC_submissions.R
@@ -65,7 +65,7 @@ source("find_unique.R")
 #####<<<< START HERE >>>>#####
 ##### Choose a sheet type (from "HIPC Dashboard.xlsx") #####
 # Available sheet_type values are "GENE", "CELLTYPE_FREQUENCY"
-sheet_type <- "GENE"
+sheet_type <- "CELLTYPE_FREQUENCY"
 
 # For the moment, assume executing interactively from the ./src directory
 source_data_dir <- "../source_data"
@@ -903,12 +903,12 @@ if (sheet_type == "GENE" && CREATE_MSIGDB) {
 if (sheet_type == "GENE") {
   # do nothing
 } else if (sheet_type == "CELLTYPE_FREQUENCY") {
-  delCols <- c("proterm_and_extra", "fully_qualified_response_component")
-  recreated_template_df <- recreated_template_df[, !colnames(recreated_template_df) %in% delCols]
+  del_cols <- c("proterm_and_extra", "fully_qualified_response_component")
+  recreated_template_df <- recreated_template_df[!colnames(recreated_template_df) %in% del_cols]
 }
 
-delCols <- c("submission_name", "submission_date", "template_name")
-recreated_template_df <- recreated_template_df[, !colnames(recreated_template_df) %in% delCols]
+del_cols <- c("submission_name", "submission_date", "template_name", "process_note")
+recreated_template_df <- recreated_template_df[!colnames(recreated_template_df) %in% del_cols]
 
 # Write out the recreated upload template in tab-delimited format
 write.table(recreated_template_df,
@@ -937,19 +937,25 @@ saveRDS(recreated_template_df,
 # NOTE - Update code if new publication URL formats are added
 df2$signature_source_url <- format_hipc_urls(df2$signature_source_url)
 
-
 # for observation summary to read correctly, need to change response_behavior
 df2$response_behavior <- sub("positive", "positively", df2$response_behavior, ignore.case = TRUE )
 df2$response_behavior <- sub("negative", "negatively", df2$response_behavior, ignore.case = TRUE )
-
-# Remove columns not needed for Dashboard.
-recreated_template_df <- recreated_template_df[!colnames(recreated_template_df) %in% c("short_comment", "process_note")]
 
 # do a final check for illegal characters in df2
 s <- strict_char_check(df2, "\xa0")  # character 160.  Dashboard loader does not like it.
 if(!is.null(s)) {
   print(paste("for df2, found problems in column (row numbers do not include any header)", s))
 }
+s <- strict_char_check(df2, "\n")    # embedded newlines
+if(!is.null(s)) {
+  print(paste("for df2, found problems in column (row numbers do not include any header)", s))
+}
+
+# Remove columns not needed for Dashboard.
+del_cols <- c("short_comment", "process_note")
+df2 <- df2[!colnames(df2) %in% del_cols]
+header_rows <- header_rows[!colnames(header_rows) %in% del_cols]
+
 
 if (sheet_type == "GENE") {
   write_submission_template(df2, header_rows, template_name, titles_and_dates_df,
